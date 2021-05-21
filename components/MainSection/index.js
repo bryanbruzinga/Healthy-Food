b8d4e229807442cefcf580c0429afcf5ff5e7919
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-05-21 06:20:13 -0300

    adicionado react-reveal

--- components/MainSection/index.js
+++ components/MainSection/index.js
@@ -1,28 +1,33 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
 import { FiSearch } from "react-icons/fi";
+import Fade from "react-reveal";
 
 export default function MainSection() {
   return (
     <section className={styles.mainSection}>
-      <div className={styles.mainContainerLeft}>
-        <h2>Ready for trying a new recipe?</h2>
-        <div>
-          <input type="text" placeholder="Search healthy recipes" />
-          <button>
-            <FiSearch />
-          </button>
+      <Fade left>
+        <div className={styles.mainContainerLeft}>
+          <h2>Ready for trying a new recipe?</h2>
+          <div>
+            <input type="text" placeholder="Search healthy recipes" />
+            <button>
+              <FiSearch />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className={styles.mainContainerRight}>
-        <Image
-          src="/Illustration.svg"
-          alt="ilustração"
-          width={760}
-          height={650}
-          objectFit="responsive"
-        />
-      </div>
+      </Fade>
+      <Fade right>
+        <div className={styles.mainContainerRight}>
+          <Image
+            src="/Illustration.svg"
+            alt="ilustração"
+            width={760}
+            height={650}
+            objectFit="responsive"
+          />
+        </div>
+      </Fade>
     </section>
   );
 }
--- components/ServicesSection/index.js
+++ components/ServicesSection/index.js
@@ -1,37 +1,42 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
+import Fade from "react-reveal";
 
 export default function ServicesSection() {
   return (
     <section className={styles.serviceContainer}>
-      <div className={styles.imgService}>
-        <Image
-          src="/bloco_services.svg"
-          alt="serviços"
-          width={600}
-          height={600}
-          objectFit="responsive"
-        />
-      </div>
-      <div className={styles.serviceDescription}>
-        <div>
-          <h2>The best services ready </h2>
-          <h2>To serve you</h2>
+      <Fade left>
+        <div className={styles.imgService}>
+          <Image
+            src="/bloco_services.svg"
+            alt="serviços"
+            width={600}
+            height={600}
+            objectFit="responsive"
+          />
         </div>
-        <p>
-          Far far away, behind the word mountains, far from the countries
-          Vokalia and Consonantia, there live the blind texts.
-        </p>
-        <p>
-          Separated they live in Bookmarksgrove right at the coast of the
-          Semantics, a large language ocean.
-        </p>
-        <p>
-          A small river named Duden flows by their place and supplies it with
-          the necessary regelialia.
-        </p>
-        <button>Know More</button>
-      </div>
+      </Fade>
+      <Fade right>
+        <div className={styles.serviceDescription}>
+          <div>
+            <h2>The best services ready </h2>
+            <h2>To serve you</h2>
+          </div>
+          <p>
+            Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts.
+          </p>
+          <p>
+            Separated they live in Bookmarksgrove right at the coast of the
+            Semantics, a large language ocean.
+          </p>
+          <p>
+            A small river named Duden flows by their place and supplies it with
+            the necessary regelialia.
+          </p>
+          <button>Know More</button>
+        </div>
+      </Fade>
     </section>
   );
 }
